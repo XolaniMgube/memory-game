a6394549393754b5debe81be846710e5ab97cbe5
xolani <xolani.mgube@umuzi.org>
2020-02-03 16:18:20 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// let toggleCardStatus = false;
+
+// function toggleCard() {
+//     let getBackCard = document.querySelector(".front");
+//     let getFrontCard = document.querySelector(".back")
+
+    
+
+//     if (toggleCardStatus === false) {
+//         getBackCard.style.display = "block";
+//         getFrontCard.style.display = "none";
+
+//         toggleCardStatus = true;
+//     }
+
+    
+// }
+
+// function toggleCard2() {
+//     let getBackCard = document.querySelector(".front");
+//     let getFrontCard = document.querySelector(".back")
+
+//     if (toggleCardStatus === true) {
+//         getBackCard.style.display = "none";
+//         getFrontCard.style.display = "block";
+
+//         toggleCardStatus = false
+//     }
+// }
+
+let clickedStatus = false;
+
+function clickBlock(e) {
+    let target = e.currentTarget
+    console.log(target.className)
+
+    // let getBlock = document.querySelector(".number")
+
+    // if (clickedStatus === false) {
+    //     getBlock.style.display = "block";
+    //     clickedStatus = true;
+    // }
+}
+
+
