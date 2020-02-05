66e70d364b68bcb9e214f0ce86f28abd930638eb
xolani <xolani.mgube@umuzi.org>
2020-02-05 15:35:42 +0200

    Wednesday almost complete

--- js/main.js
+++ js/main.js
@@ -1,29 +1,60 @@
 let clickedCard = null;
+let preventClick = false;
+let matches = 0;
 
-function onCardClicked(e) {
-    let target = e.currentTarget
-    target.className = target.className.replace("hide-color", "")
+let colors = ["red", "blue", "orange", "pink", "yellow", "brown", "green", "black"]
 
-    target.className += " done"
+let cards = [...document.querySelectorAll(".card")];
+for (let color of colors) {
+    const cardAIndex = parseInt(Math.random() * cards.length);
+    const cardA = cards[cardAIndex];
+    cards.splice(cardAIndex, 1);
+    cardA.className += ` ${color}`
+    cardA.setAttribute("data-color", color);
 
-    console.log(target.getAttribute("data-color"))
 
-    if (!clickedCard) {
-        clickedCard = target;
-    } else if (clickedCard) {
-        if (clickedCard.getAttribute("data-color") === target.getAttribute("data-color")) {
-            console.log("cards ARE equal")
-            clickedCard = null
-        } else{
-            console.log("cards NOT equal")
-            setTimeout(() => {
-                console.log("we are here!!")
-                clickedCard.className = clicked.className.replace("done", "").trim() + " hide-color"
-                target.className =
-            })
-        }
-    }
-    
+    const cardBIndex = parseInt(Math.random() * cards.length);
+    const cardB = cards[cardBIndex];
+    cards.splice(cardBIndex, 1);
+    cardB.className += ` ${color}`
+    cardB .setAttribute("data-color", color);
 }
 
 
+function card(event) {
+    let target = event.currentTarget
+
+    if (preventClick || target === clickedCard || target.className.includes("done")) {
+        return;
+    }
+
+    target.classList.remove("hide-color")
+    target.className += " done"
+    console.log(target.getAttribute("data-color"))
+    
+
+    
+    if (!clickedCard) {
+        clickedCard = target
+    } else if (clickedCard) {
+        if (clickedCard.getAttribute("data-color") === target.getAttribute("data-color")) {
+            matches++
+            clickedCard = null;
+            if (matches == 8) {
+                alert("You Won");
+            }
+            console.log("cards ARE equal")
+        }
+        else {
+            preventClick = true;
+            console.log("cards NOT equal")
+            setTimeout(() => {
+                clickedCard.className = clickedCard.className.replace("done", "") + " hide-color";
+                target.className =  target.className.replace("done", "") + " hide-color";
+                clickedCard = null;
+                preventClick = false;
+            }, 500)
+        }
+    }
+     
+}
